d9bb303de7d9618e54e1a1668298bf97c68fbb32
ghostchez <cborchez9@hotmail.com>
2023-09-19 18:17:24 -0300

    Ultimos avances pre-entrega 1

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let usuariosController = require("../controller/usuarios");
 let userRolValidation = require("../middlewares/userRolValidation");
 
-router.get("/mi_cuenta",usuariosController.mi_cuenta);
+router.get("/mi_cuenta",userRolValidation("any"),usuariosController.mi_cuenta);
 router.get("/datos_pago",usuariosController.datos_pago);
 router.get("/mis_ordenes",usuariosController.mis_ordenes);
 
